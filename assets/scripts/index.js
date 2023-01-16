79e63bc5797c63f580a0a8eba6b2cb3e58fa40a5
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-01-16 13:58:21 -0400

    import date in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,2 +1,5 @@
+import time from './modules/date.js';
+
 export { addBook, remove } from './modules/book_funtionalities.js';
 export { list, addNew, contact } from './modules/navigation.js';
+export { time };
